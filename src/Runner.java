6a3242e62635fcfb638b6267366e99e88c73a982
dylward757 <dylward757@gmail.com>
2017-05-17 12:16:18 -0600

    Added JOP Welcoming message

--- src/Runner.java
+++ src/Runner.java
@@ -1,9 +1,12 @@
+import javax.swing.JOptionPane;
 
 public class Runner {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Runner myRun = new Runner();
+		Converter myConverter = new Converter();
+		JOptionPane.showMessageDialog(null, "Welcome, this programe converts X to Y");
 	}
 
 }
