4d9a28db0ba1cf5d1086fd6bf78bc3a084304df4
dylward757 <dylward757@gmail.com>
2017-05-17 12:31:50 -0600

    Added JOP greeting

--- src/Runner.java
+++ src/Runner.java
@@ -1,8 +1,11 @@
+import javax.swing.JOptionPane;
 
 public class Runner {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Runner myRun = new Runner();
+		Converter myConverter = new Converter();
+		JOptionPane.showMessageDialog(null, "Welcome, This is a greeting");
 
 	}
 
